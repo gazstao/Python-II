d508511a31318efc4696254f027ea4b07115a87c
gazstao <gazstao@gmail.com>
2023-10-08 11:33:37 -0300

    Updated help messages

--- RansomWhere.py
+++ RansomWhere.py
@@ -11,9 +11,7 @@ import pathlib
 import secrets
 import os
 import base64
-import sys
-import getpass
-import cryptography
+import sys555
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 
