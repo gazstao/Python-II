492ad78743a69a8643aa4e13db1cf2224fbbf298
gazstao <gazstao@gmail.com>
2023-10-13 20:55:17 -0300

    clock feature added

--- clock.py
+++ clock.py
@@ -8,10 +8,10 @@ listadefontes = pyfiglet.FigletFont.getFonts()
 while True:
     print("\n\n\n\n")
     hora_atual = datetime.datetime.now()
-    hora_formatada_1 = (str(hora_atual.day)+" "+str(hora_atual.month)+" "+str(hora_atual.year))
-    hora_formatada_2 = (str(hora_atual.hour) + " " + str(hora_atual.minute) + " " + str(hora_atual.second))
+    hora_formatada_1 = f"{hora_atual.day} {hora_atual.month} {hora_atual.year}"
+    hora_formatada_2 = f"{hora_atual.hour} {hora_atual.minute} {hora_atual.second}"
 
     fonte = random.choice(listadefontes)
     pyfiglet.print_figlet(hora_formatada_1, fonte, "green", width=80)
-    pyfiglet.print_figlet(hora_formatada_2, fonte, "green", width=80)
-    time.sleep(5)
+    pyfiglet.print_figlet(hora_formatada_2, fonte, "blue", width=80)
+    time.sleep(13)
