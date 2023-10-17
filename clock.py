4a440c6bbce07264e0c5de04ea3ff3854bd2ef0a
gazstao <gazstao@gmail.com>
2023-10-17 15:01:57 -0300

    clock.py simplified

--- clock.py
+++ clock.py
@@ -8,7 +8,6 @@ import keyboard
 listadefontes = pyfiglet.FigletFont.getFonts()
 fonte = "basic"
 largura = 150
-banner = "gazstao       "
 tam = len(banner)
 
 while True:
@@ -16,8 +15,6 @@ while True:
     hora_atual = datetime.datetime.now()
     hora_formatada_1 = hora_atual.strftime("%d %b %y")
     hora_formatada_2 = hora_atual.strftime("%H %M %S")
-    #hora_formatada_1 = f"{hora_atual.day} {hora_atual.month} {hora_atual.year}"
-    #hora_formatada_2 = f"{hora_atual.hour} {hora_atual.minute} {hora_atual.second}"
 
     if (keyboard.is_pressed("enter") | keyboard.is_pressed(" ")):
         fonte = random.choice(listadefontes)
@@ -25,7 +22,4 @@ while True:
     os.system('cls')
     pyfiglet.print_figlet(hora_formatada_1, fonte, "green", width=largura, justify="center")
     pyfiglet.print_figlet(hora_formatada_2, fonte, "blue", width=largura, justify="center")
-    pyfiglet.print_figlet(banner, fonte, "red", width=largura, justify="center")
-    # rolling banner
-    banner = f"{banner[tam-1:]}{banner[0:tam-1]}"
     time.sleep(1)
